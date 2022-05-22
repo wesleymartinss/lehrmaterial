a83a744aa99b60e8e61ecb276422e91d3da7f24e
Gabriel de Oliveira Koehler <gabriel_koehler@hotmail.com>
2022-05-22 14:41:53 -0300

    setup backend for devise api authentication

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Users::SessionsController < Devidse::SessionsControllers
+  respond_to :json
+
+  private
+
+  def respond_with(_resource, _opts = {})
+    render json: {
+      message: 'You are logged in.'
+      user: current_user
+    }, status: ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: 'You are logged out.' }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: 'Nothing happened.' }, status: :unauthorized
+  end
+end
